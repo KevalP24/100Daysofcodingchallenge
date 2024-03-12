b837c99180b4baa2de2623afc89ae7f452934029
KevalP24 <118961649+KevalP24@users.noreply.github.com>
2024-03-12 20:39:20 +0530

    3 Problems

--- Day-2/876_MiddleoftheLinkedList.py
+++ Day-2/876_MiddleoftheLinkedList.py
@@ -1,3 +1,4 @@
+LEETCODE SOLUTION:
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -11,3 +12,30 @@ class Solution:
             slow=slow.next
             fast=fast.next.next
         return slow
+
+FULL CODE:
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+def main():
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+
+    solution = Solution()
+    middle_node = solution.middleNode(head)
+    if middle_node:
+        print("Middle node has value:", middle_node.val)
+    else:
+        print("No middle node.")
+
+if __name__ == "__main__":
+    main()
