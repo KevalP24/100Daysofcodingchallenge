b6f77c53b33d454289949189543cf850b321e81e
KevalP24 <118961649+KevalP24@users.noreply.github.com>
2024-03-12 20:30:16 +0530

    Update 1929_ConcatenationofArray.py

--- Day-1/1929_ConcatenationofArray.py
+++ Day-1/1929_ConcatenationofArray.py
@@ -1,3 +1,20 @@
+LEETCODE SOLUTION:
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums+nums
+
+FULL CODE:
+from typing import List
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+
+def main():
+    nums = [1, 2, 3]
+    solution = Solution()
+    concatenated_nums = solution.getConcatenation(nums)
+    print(concatenated_nums)
+
+if __name__ == "__main__":
+    main()
