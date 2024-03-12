81d11e55022c619ea830301cc5fdbe6ffd280b21
KevalP24 <118961649+KevalP24@users.noreply.github.com>
2024-03-12 20:36:48 +0530

    Update 11_ContainerWithMostWater.py

--- Day-2/11_ContainerWithMostWater.py
+++ Day-2/11_ContainerWithMostWater.py
@@ -1,3 +1,4 @@
+LEETCODE SOLUTION:
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         n=len(height)
@@ -13,3 +14,31 @@ class Solution:
             else:
                 j-=1
         return ans
+
+FULL CODE:
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        i = 0
+        j = n - 1
+        ans = 0
+        while i < j:
+            area = (j - i) * min(height[i], height[j])
+            ans = max(area, ans)
+
+            if height[i] <= height[j]:
+                i += 1
+            else:
+                j -= 1
+        return ans
+
+def main():
+    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    solution = Solution()
+    max_area = solution.maxArea(height)
+    print("Max area:", max_area)
+
+if __name__ == "__main__":
+    main()
