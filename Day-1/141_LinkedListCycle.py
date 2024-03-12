a1f58cb32b8cdba8a258909a1e41a085be9cad7d
KevalP24 <118961649+KevalP24@users.noreply.github.com>
2024-03-12 20:29:25 +0530

    Update 141_LinkedListCycle.py

--- Day-1/141_LinkedListCycle.py
+++ Day-1/141_LinkedListCycle.py
@@ -1,8 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+LEETCODE SOLUTION:
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while True:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False 
+
+FULL CODE:
+from typing import Optional
+
+class ListNode:
+    def __init__(self, x: int):
+        self.val = x
+        self.next = None
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
@@ -16,3 +32,15 @@ class Solution:
             if slow == fast:
                 return True
         return False 
+
+def main():
+    head = ListNode(3)
+    head.next = ListNode(2)
+    head.next.next = ListNode(0)
+    head.next.next.next = ListNode(-4)
+    head.next.next.next.next = head.next
+    solution = Solution()
+    print(solution.hasCycle(head))
+
+if __name__ == "__main__":
+    main()
