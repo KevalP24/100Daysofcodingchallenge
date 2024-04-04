441d2febd90b222669def0721f07f2d28ec5994e
KevalP24 <118961649+KevalP24@users.noreply.github.com>
2024-04-04 18:44:18 +0530

    2 Problems

--- Day-30/118_Pascal'sTriangle.py
+++ Day-30/118_Pascal'sTriangle.py
@@ -4,8 +4,10 @@ class Solution:
             return []
         if numRows == 1:
             return [[1]]
+            
         ans = self.generate(numRows - 1)
         temp = [1] * numRows
+        
         for i in range(1, numRows - 1):
             temp[i] = ans[-1][i - 1] + ans[-1][i]
         
