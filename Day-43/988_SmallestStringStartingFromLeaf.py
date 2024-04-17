a64e4bd1aebd104e625d8270995e6ef85e192672
KevalP24 <118961649+KevalP24@users.noreply.github.com>
2024-04-17 18:08:17 +0530

    Create 988_SmallestStringStartingFromLeaf.py

---
+++ Day-43/988_SmallestStringStartingFromLeaf.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.result = ""
+    
+    def solve(self, root, curr):
+        if not root:
+            return
+        
+        curr = chr(root.val + ord('a')) + curr
+        
+        if not root.left and not root.right:
+            if not self.result or self.result > curr:
+                self.result = curr
+            return
+        
+        self.solve(root.left, curr)
+        self.solve(root.right, curr)
+        
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        self.solve(root, "")
+        return self.result
