4ed1abeefa7fbd3ce0ba029287d226033127ebba
KevalP24 <118961649+KevalP24@users.noreply.github.com>
2024-05-11 19:14:24 +0530

    2 Problems

--- Day-67/145_BinaryTreePostorderTraversal.py
+++ Day-67/145_BinaryTreePostorderTraversal.py
@@ -6,6 +6,8 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
         if root==None:
             return []
+            
         return self.postorderTraversal(root.left)+self.postorderTraversal(root.right)+[root.val]
