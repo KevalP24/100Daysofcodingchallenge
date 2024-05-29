c53adfc400bade449177cd865eec19a9ee2e8f07
KevalP24 <118961649+KevalP24@users.noreply.github.com>
2024-05-29 10:48:42 +0530

    1 Problem

---
+++ Day-85/2974_MinimumNumberGame.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        l =len(nums)
+
+        for i in range (0,l,2):
+            nums[i] ,nums[i+1] = nums[i+1] ,nums[i]
+        
+        return nums
